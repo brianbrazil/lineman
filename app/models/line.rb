69f6c4017be4956ef327b3524d087e8ee341236c
Brian Brazil <brian.brazil@teespring.com>
2014-04-19 11:06:56 -0400

    Added relationships between events, lines, and rooms

--- app/models/line.rb
+++ app/models/line.rb
@@ -1,6 +1,8 @@
 class Line
   include Mongoid::Document
 
+  belongs_to :room
+
   field :name, type: String
   field :length, type: Integer
 
--- app/models/room.rb
+++ app/models/room.rb
@@ -1,6 +1,9 @@
 class Room
   include Mongoid::Document
 
+  belongs_to :event
+  has_many :lines
+
   field :name, type: String
   field :capacity, type: Integer
 
