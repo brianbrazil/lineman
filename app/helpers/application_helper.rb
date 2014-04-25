873833da9bbcee086f08567a3176107eac4a0136
Brian Brazil <brian.brazil@teespring.com>
2014-04-25 09:33:28 -0400

    Created link_with_icon

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,7 @@
 module ApplicationHelper
+
+    def link_with_icon(icon_class = nil, name = nil, options = nil, html_options = nil, &block)
+      link_to("<i class=#{icon_class}></i> #{ name }".html_safe, options, html_options, &block)
+    end
+
 end
