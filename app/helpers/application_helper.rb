3b93f4f3fb04b338eacbc17515f8756f6a85470a
Brian Brazil <brian.brazil@teespring.com>
2014-05-02 09:29:29 -0400

    Added flash upon create

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,16 @@
 module ApplicationHelper
 
-    def link_with_icon(icon_class = nil, name = nil, options = nil, html_options = nil, &block)
-      link_to("<i class=#{icon_class}></i> #{ name }".html_safe, options, html_options, &block)
+  def link_with_icon(icon_class = nil, name = nil, options = nil, html_options = nil, &block)
+    link_to("<i class=#{icon_class}></i> #{ name }".html_safe, options, html_options, &block)
+  end
+
+  def flash_class(level)
+    case level
+      when :notice then "flash alert alert-success"
+      when :success then "flash alert alert-success"
+      when :error then "flash alert alert-danger"
+      when :alert then "flash alert alert-warning"
     end
+  end
 
 end
