37e2d458aa7993beea53034306f9834e1ba3366c
Brian Brazil <brian.brazil@teespring.com>
2014-05-15 20:21:25 -0400

    Use qrcode gem

--- Gemfile
+++ Gemfile
@@ -18,6 +18,7 @@ gem 'sass-rails', '~> 4.0.0'
 gem 'uglifier', '>= 1.3.0'
 
 gem 'draper', '~> 1.3'
+gem 'qrcode_web'
 
 gem 'jquery-rails'
 gem 'jquery-ui-rails'
--- app/decorators/event_decorator.rb
+++ app/decorators/event_decorator.rb
@@ -1,11 +1,6 @@
 class EventDecorator < Draper::Decorator
   delegate_all
 
-  def qrcode(size)
-    "http://chart.apis.google.com/chart?cht=qr&chs=#{size}x#{size}&chl=#{URI.escape(h.event_url(object))}&chld=H|0"
-  end
-
-
   # Define presentation-specific methods here. Helpers are accessed through
   # `helpers` (aka `h`). You can override attributes, for example:
   #
